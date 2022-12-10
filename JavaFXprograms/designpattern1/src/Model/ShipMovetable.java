b04b71f2e2931a1ce9b2adeb671d615c961be76b
Omri Yssef <omriyssef@Omris-MacBook-Pro.local>
2022-12-10 14:14:17 +0200

    adding projecs

---
+++ JavaFXprograms/designpattern1/src/Model/ShipMovetable.java
@@ -0,0 +1,7 @@
+package Model;
+
+public interface ShipMovetable   {
+	public Containerr getTopContainerByIndex(int xx , int yy );
+	public Containerr getTopContainer() ;
+	public void addContainers(Containerr container);
+}
