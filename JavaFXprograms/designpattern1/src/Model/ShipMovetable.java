4fb1793265f25dfdc8b9efc4f9a1ebdadfcd4552
Chenomri <96019991+Chenomri@users.noreply.github.com>
2022-12-10 16:39:24 +0200

    Delete JavaFXprograms/designpattern1 directory

--- JavaFXprograms/designpattern1/src/Model/ShipMovetable.java
+++
@@ -1,7 +0,0 @@
-package Model;
-
-public interface ShipMovetable   {
-	public Containerr getTopContainerByIndex(int xx , int yy );
-	public Containerr getTopContainer() ;
-	public void addContainers(Containerr container);
-}
